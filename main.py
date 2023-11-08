60e7d9659e477c33fa02b954f0038c7e7abd8f13
VasekHolub <vholub33@gmail.com>
2023-11-08 16:39:22 +0100

    Simplified code. Strings including "-" made viable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 
 
 def exclusion_list_load(file_path: str) -> list:
@@ -32,43 +33,34 @@ def find_indices(list_to_check: list, item_to_find: str) -> list:
 def reference_mark_extractor(extraction_source: list) -> list:
     ref_marks = list()
     for i in extraction_source:
-        if i not in ref_marks:
+        if i not in ref_marks and i not in exclusion_list_load(
+            os.path.join("exclusion_list.txt")
+        ):
             for k in find_indices(extraction_source, i):
                 if k + 1 >= len(extraction_source):
                     break
                 elif extraction_source[k + 1] == "":
                     continue
-                elif extraction_source[k + 1][0].isnumeric():
+                elif (
+                    extraction_source[k + 1][0].isnumeric()
+                    and i not in ref_marks
+                    and not i.isnumeric()
+                ):
                     ref_marks.append(i)
     return ref_marks
 
 
-def ref_mark_repetitons_remover(ref_marks: list) -> list:
-    unique_ref_marks = list()
-    ref_marks_set = set(ref_marks)
-    for i in ref_marks_set:
-        if (
-            not i.isnumeric()
-            and i.isalnum()
-            and i not in exclusion_list_load(os.path.join("exclusion_list.txt"))
-        ):
-            unique_ref_marks.append(i)
-    unique_ref_marks.sort()
-    return unique_ref_marks
-
-
-def unique_ref_marks_csv_exporter(unique_ref_marks: list):
+def ref_marks_csv_exporter(ref_marks: list):
     with open("csv_file.csv", mode="w", encoding="utf-8-sig", newline="") as file:
         writer = csv.writer(file)
-        for item in unique_ref_marks:
+        for item in ref_marks:
             writer.writerow([item])
 
 
 def main():
     contents_list = string_list_stripper(txt_file_load(os.path.join("file.txt")))
     ref_marks = reference_mark_extractor(contents_list)
-    unique_ref_marks = ref_mark_repetitons_remover(ref_marks)
-    unique_ref_marks_csv_exporter(unique_ref_marks)
+    ref_marks_csv_exporter(ref_marks)
 
 
 if __name__ == "__main__":
